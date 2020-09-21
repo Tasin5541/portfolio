5ed14bf53df1307422d1612df76277bd294b6074
Tasin5541 <42533823+Tasin5541@users.noreply.github.com>
2020-09-21 15:33:03 +0000

    Deploying to gh-pages from  @ 49d87cf297cb18c755ca59d276b261c90715db04 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.2273376c4853c9d834cffc51929ef4c0.js"
+  "/portfolio/precache-manifest.1bd15abf1b40e2caed5fe12b8df07c69.js"
 );
 
 self.addEventListener('message', (event) => {
