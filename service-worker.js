11940730c083c8fb21543fa4d48478d4fce3e99e
Miftaul Mannan Tasin <tasinmiftaulmannan@gmail.com>
2020-09-22 11:03:17 +0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.1bd15abf1b40e2caed5fe12b8df07c69.js"
+  "/portfolio/precache-manifest.12e26b4ee0de1e2c8725f6b17328b0a0.js"
 );
 
 self.addEventListener('message', (event) => {
