bfc19d18e9c6485c25d01a6869b68e6e20b505b8
Tasin5541 <42533823+Tasin5541@users.noreply.github.com>
2020-09-29 16:11:26 +0000

    Deploying to gh-pages from  @ 9439a8e622e3eaa08a5ef78d92270dc18a0100e7 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f080a051be707f8607cd57be05870eef.js"
+  "/portfolio/precache-manifest.4097547a54f0d24fdf4256de551d31a5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
